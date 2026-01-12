410e1242cdae4da9bf922f49c1fe967cdde801ec
rara567 <konamison1@gmail.com>
2026-01-12 22:02:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,60 +1,135 @@
-/* ===============================
-   Initialize Map
-================================ */
-var map = L.map('map').setView([3.139, 101.6869], 12);
+// ===============================
+// Koordinat Politeknik Ungku Omar
+// ===============================
+var puoLatLng = [4.5975, 101.0901];
 
-/* ===============================
-   Basemap Layers
-================================ */
+// ===============================
+// Initialize Map (AUTO FOCUS PUO)
+// ===============================
+var map = L.map('map', {
+  zoomControl: true
+}).setView(puoLatLng, 18);
+
+// ===============================
+// Basemap
+// ===============================
 var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '© OpenStreetMap Contributors'
+}).addTo(map);
+
+// ===============================
+// Marker PUO (Auto Highlight)
+// ===============================
+var puoMarker = L.marker(puoLatLng)
+  .addTo(map)
+  .bindPopup("<b>Politeknik Ungku Omar</b><br>Ipoh, Perak")
+  .openPopup();
+
+// ===============================
+// Layer Containers
+// ===============================
+var bangunanLayer = L.geoJSON(null, {
+  style: {
+    color: "#0b5ed7",
+    weight: 2,
+    fillOpacity: 0.6
+  },
+  onEachFeature: function (feature, layer) {
+    layer.bindPopup(
+      "<b>" + feature.properties.nama + "</b><br>" +
+      feature.properties.keterangan
+    );
+  }
 });
 
-var satellite = L.tileLayer(
-  'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-  { attribution: '© Esri' }
-);
+var fasilitiLayer = L.geoJSON(null, {
+  pointToLayer: function (feature, latlng) {
+    return L.circleMarker(latlng, {
+      radius: 6,
+      fillColor: "#198754",
+      color: "#000",
+      weight: 1,
+      fillOpacity: 0.8
+    }).bindPopup("<b>" + feature.properties.nama + "</b>");
+  }
+});
 
-osm.addTo(map);
+// ===============================
+// Load Data
+// ===============================
+fetch('data/bangunan.geojson')
+  .then(res => res.json())
+  .then(data => {
+    bangunanLayer.addData(data);
+    bangunanLayer.addTo(map);
+  });
 
-/* ===============================
-   Layer Control (Basemap Only)
-================================ */
-var baseMaps = {
-  "OpenStreetMap": osm,
-  "Satellite": satellite
-};
+fetch('data/fasiliti.geojson')
+  .then(res => res.json())
+  .then(data => {
+    fasilitiLayer.addData(data);
+  });
 
-L.control.layers(baseMaps, null, { collapsed: false }).addTo(map);
+// ===============================
+// Layer Control
+// ===============================
+L.control.layers(null, {
+  "Bangunan Kampus": bangunanLayer,
+  "Fasiliti": fasilitiLayer
+}, { collapsed: false }).addTo(map);
 
-/* ===============================
-   Legend (Static / Example)
-================================ */
-var legend = L.control({ position: 'bottomright' });
-
-legend.onAdd = function () {
-  var div = L.DomUtil.create('div', 'legend');
-  div.innerHTML += "<b>Legend (Sample)</b><br>";
-  div.innerHTML += '<i style="background:#1f78b4"></i> Kawasan Kajian<br>';
-  div.innerHTML += '<i style="background:#33a02c"></i> Infrastruktur<br>';
-  div.innerHTML += '<i style="background:#e31a1c"></i> Zon Sensitif<br>';
-  return div;
-};
-
-legend.addTo(map);
-
-/* ===============================
-   Measure Tool
-================================ */
+// ===============================
+// Measure Tool
+// ===============================
 L.control.measure({
-  position: 'topleft',
   primaryLengthUnit: 'meters',
   secondaryLengthUnit: 'kilometers',
   primaryAreaUnit: 'sqmeters',
   secondaryAreaUnit: 'hectares'
 }).addTo(map);
 
-/* ===============================
-   Scale Bar
-================================ */
+// ===============================
+// Scale Bar
+// ===============================
 L.control.scale().addTo(map);
+
+// ===============================
+// Search Location (Global)
+// ===============================
+var searchMarker;
+
+L.Control.geocoder({
+  defaultMarkGeocode: false,
+  placeholder: 'Cari lokasi (contoh: Hospital, Bandar, Alamat)'
+})
+.on('markgeocode', function(e) {
+  var center = e.geocode.center;
+
+  if (searchMarker) {
+    map.removeLayer(searchMarker);
+  }
+
+  map.setView(center, 17);
+  searchMarker = L.marker(center)
+    .addTo(map)
+    .bindPopup(e.geocode.name)
+    .openPopup();
+})
+.addTo(map);
+
+// ===============================
+// Legend
+// ===============================
+var legend = L.control({ position: 'bottomright' });
+
+legend.onAdd = function () {
+  var div = L.DomUtil.create('div', 'legend');
+  div.innerHTML += "<b>Legenda</b><br>";
+  div.innerHTML += '<i style="background:#0b5ed7"></i> Bangunan<br>';
+  div.innerHTML += '<i style="background:#198754"></i> Fasiliti<br>';
+  div.innerHTML += '<i style="background:#ff0000"></i> Lokasi Carian<br>';
+  return div;
+};
+
+legend.addTo(map);
+
