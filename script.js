f596644221bb8e9fc3e6572c4ec3cbdcbf1c9d2f
rara567 <konamison1@gmail.com>
2026-01-14 08:21:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 // ===============================
-// Koordinat Politeknik Ungku Omar
+// Koordinat PUO (4 titik perpuluhan)
 // ===============================
-var puoLatLng = [4.588556, 101.126056];
+var puoLatLng = [4.5886, 101.1261];
 
 // ===============================
-// Initialize Map (AUTO FOCUS PUO)
+// Initialize Map
 // ===============================
-var map = L.map('map', {
-  zoomControl: true
-}).setView(puoLatLng, 18);
+var map = L.map('map').setView(puoLatLng, 18);
 
 // ===============================
 // Basemap
@@ -18,30 +16,38 @@ var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // ===============================
-// Marker PUO (Auto Highlight)
+// Marker PUO
 // ===============================
-var puoMarker = L.marker(puoLatLng)
+L.marker(puoLatLng)
   .addTo(map)
   .bindPopup("<b>Politeknik Ungku Omar</b><br>Ipoh, Perak")
   .openPopup();
 
 // ===============================
-// Layer Containers
+// Bangunan Layer (Polygon)
 // ===============================
 var bangunanLayer = L.geoJSON(null, {
   style: {
     color: "#0b5ed7",
     weight: 2,
-    fillOpacity: 0.6
+    fillColor: "#0b5ed7",
+    fillOpacity: 0.5
   },
   onEachFeature: function (feature, layer) {
     layer.bindPopup(
-      "<b>" + feature.properties.nama + "</b><br>" +
-      feature.properties.keterangan
+      "<b>" + (feature.properties.nama || "Bangunan") + "</b><br>" +
+      (feature.properties.keterangan || "")
     );
+
+    layer.on('click', function () {
+      map.fitBounds(layer.getBounds());
+    });
   }
 });
 
+// ===============================
+// Fasiliti Layer (Point)
+// ===============================
 var fasilitiLayer = L.geoJSON(null, {
   pointToLayer: function (feature, latlng) {
     return L.circleMarker(latlng, {
@@ -55,20 +61,15 @@ var fasilitiLayer = L.geoJSON(null, {
 });
 
 // ===============================
-// Load Data
+// Load GeoJSON Data
 // ===============================
 fetch('data/bangunan.geojson')
   .then(res => res.json())
-  .then(data => {
-    bangunanLayer.addData(data);
-    bangunanLayer.addTo(map);
-  });
+  .then(data => bangunanLayer.addData(data).addTo(map));
 
 fetch('data/fasiliti.geojson')
   .then(res => res.json())
-  .then(data => {
-    fasilitiLayer.addData(data);
-  });
+  .then(data => fasilitiLayer.addData(data).addTo(map));
 
 // ===============================
 // Layer Control
@@ -94,22 +95,21 @@ L.control.measure({
 L.control.scale().addTo(map);
 
 // ===============================
-// Search Location (Global)
+// Search (Global)
 // ===============================
 var searchMarker;
 
 L.Control.geocoder({
   defaultMarkGeocode: false,
-  placeholder: 'Cari lokasi (contoh: Hospital, Bandar, Alamat)'
+  placeholder: 'Cari lokasi'
 })
 .on('markgeocode', function(e) {
   var center = e.geocode.center;
 
-  if (searchMarker) {
-    map.removeLayer(searchMarker);
-  }
+  if (searchMarker) map.removeLayer(searchMarker);
 
-  map.setView(center, 17);
+  map.flyTo(center, 17, { duration: 1.5 });
+
   searchMarker = L.marker(center)
     .addTo(map)
     .bindPopup(e.geocode.name)
@@ -127,10 +127,8 @@ legend.onAdd = function () {
   div.innerHTML += "<b>Legenda</b><br>";
   div.innerHTML += '<i style="background:#0b5ed7"></i> Bangunan<br>';
   div.innerHTML += '<i style="background:#198754"></i> Fasiliti<br>';
-  div.innerHTML += '<i style="background:#ff0000"></i> Lokasi Carian<br>';
   return div;
 };
 
 legend.addTo(map);
-
 
