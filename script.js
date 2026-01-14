71652c9bfbad57aed51e8826f0d803da0ee81dc4
rara567 <konamison1@gmail.com>
2026-01-14 09:45:05 +0800

    Implement custom zoom control and enhance tracking
    Added custom zoom control and improved user location tracking.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ===============================
-// Koordinat PUO (4 titik perpuluhan)
+// Koordinat PUO
 // ===============================
 var puoLatLng = [4.5886, 101.1261];
 
@@ -9,9 +9,61 @@ var puoLatLng = [4.5886, 101.1261];
 var map = L.map('map', {
   center: puoLatLng,
   zoom: 18,
-  maxZoom: 20,  // max zoom untuk pengguna
-  minZoom: 5    // min zoom
+  maxZoom: 19,
+  minZoom: 5,
+  zoomControl: false
 });
+
+// ===============================
+// Custom Zoom Control Besar + Fokus PUO
+// ===============================
+var customZoom = L.control({ position: 'topright' });
+
+customZoom.onAdd = function(map) {
+  var div = L.DomUtil.create('div', 'custom-zoom');
+  div.style.background = 'white';
+  div.style.padding = '5px';
+  div.style.borderRadius = '5px';
+  div.style.boxShadow = '0 0 5px rgba(0,0,0,0.3)';
+  div.style.display = 'flex';
+  div.style.flexDirection = 'column';
+  div.style.alignItems = 'center';
+  
+  // Zoom In
+  var zoomIn = L.DomUtil.create('button', '', div);
+  zoomIn.innerHTML = '+';
+  zoomIn.style.fontSize = '25px';
+  zoomIn.style.width = '50px';
+  zoomIn.style.height = '50px';
+  zoomIn.style.marginBottom = '5px';
+  zoomIn.style.cursor = 'pointer';
+
+  // Zoom Out
+  var zoomOut = L.DomUtil.create('button', '', div);
+  zoomOut.innerHTML = '-';
+  zoomOut.style.fontSize = '25px';
+  zoomOut.style.width = '50px';
+  zoomOut.style.height = '50px';
+  zoomOut.style.cursor = 'pointer';
+
+  // Fokus PUO
+  var focusPUO = L.DomUtil.create('button', '', div);
+  focusPUO.innerHTML = 'PUO';
+  focusPUO.style.fontSize = '18px';
+  focusPUO.style.width = '50px';
+  focusPUO.style.height = '50px';
+  focusPUO.style.marginTop = '5px';
+  focusPUO.style.cursor = 'pointer';
+
+  // Event Buttons
+  L.DomEvent.on(zoomIn, 'click', function(e) { map.zoomIn(); });
+  L.DomEvent.on(zoomOut, 'click', function(e) { map.zoomOut(); });
+  L.DomEvent.on(focusPUO, 'click', function(e) { map.flyTo(puoLatLng, 18, {animate:true,duration:1.2}); });
+
+  return div;
+};
+
+customZoom.addTo(map);
 
 // ===============================
 // Basemap
@@ -29,32 +81,21 @@ L.marker(puoLatLng)
   .openPopup();
 
 // ===============================
-// Bangunan Layer (Polygon)
+// Bangunan Layer
 // ===============================
 var bangunanLayer = L.geoJSON(null, {
-  style: {
-    color: "#0b5ed7",
-    weight: 2,
-    fillColor: "#0b5ed7",
-    fillOpacity: 0.5
-  },
-  onEachFeature: function (feature, layer) {
-    layer.bindPopup(
-      "<b>" + (feature.properties.nama || "Bangunan") + "</b><br>" +
-      (feature.properties.keterangan || "")
-    );
-
-    layer.on('click', function () {
-      map.fitBounds(layer.getBounds());
-    });
+  style: { color: "#0b5ed7", weight: 2, fillColor: "#0b5ed7", fillOpacity: 0.5 },
+  onEachFeature: function(feature, layer) {
+    layer.bindPopup("<b>" + (feature.properties.nama || "Bangunan") + "</b><br>" + (feature.properties.keterangan || ""));
+    layer.on('click', function() { map.fitBounds(layer.getBounds()); });
   }
 });
 
 // ===============================
-// Fasiliti Layer (Point)
+// Fasiliti Layer
 // ===============================
 var fasilitiLayer = L.geoJSON(null, {
-  pointToLayer: function (feature, latlng) {
+  pointToLayer: function(feature, latlng) {
     return L.circleMarker(latlng, {
       radius: 6,
       fillColor: "#198754",
@@ -66,7 +107,7 @@ var fasilitiLayer = L.geoJSON(null, {
 });
 
 // ===============================
-// Load GeoJSON Data
+// Load GeoJSON
 // ===============================
 fetch('data/bangunan.geojson')
   .then(res => res.json())
@@ -100,9 +141,9 @@ L.control.measure({
 L.control.scale().addTo(map);
 
 // ===============================
-// Search (Global)
+// Search
 // ===============================
-var searchMarker;
+var searchMarker; // Hanya satu marker search
 
 L.Control.geocoder({
   defaultMarkGeocode: false,
@@ -111,14 +152,15 @@ L.Control.geocoder({
 .on('markgeocode', function(e) {
   var center = e.geocode.center;
 
-  if (searchMarker) map.removeLayer(searchMarker);
-
-  map.flyTo(center, 17, { duration: 1.5 });
+  // Hapus marker search lama jika ada
+  if(searchMarker) map.removeLayer(searchMarker);
 
   searchMarker = L.marker(center)
     .addTo(map)
     .bindPopup(e.geocode.name)
     .openPopup();
+
+  map.flyTo(center, 17, {duration:1.5});
 })
 .addTo(map);
 
@@ -126,50 +168,37 @@ L.Control.geocoder({
 // Legend
 // ===============================
 var legend = L.control({ position: 'bottomright' });
-
-legend.onAdd = function () {
+legend.onAdd = function() {
   var div = L.DomUtil.create('div', 'legend');
   div.innerHTML += "<b>Legenda</b><br>";
-  div.innerHTML += '<i style="background:#0b5ed7"></i> Bangunan<br>';
-  div.innerHTML += '<i style="background:#198754"></i> Fasiliti<br>';
-  div.innerHTML += '<i style="background:#136aec"></i> Lokasi Pengguna<br>';
+  div.innerHTML += '<i style="background:#0b5ed7;width:15px;height:15px;display:inline-block;margin-right:5px;"></i> Bangunan<br>';
+  div.innerHTML += '<i style="background:#198754;width:15px;height:15px;display:inline-block;margin-right:5px;"></i> Fasiliti<br>';
+  div.innerHTML += '<i style="background:#136aec;width:15px;height:15px;display:inline-block;margin-right:5px;"></i> Lokasi Pengguna<br>';
   return div;
 };
-
 legend.addTo(map);
 
-// ===================================================
-// 🔴 USER LOCATION TRACKING (MAP FOLLOW USER)
-// ===================================================
+// ===============================
+// User Location Tracking (Pin Tetap)
+// ===============================
 var userMarker, accuracyCircle;
-var followUser = true; // true = map auto-zoom ikut pengguna
-var currentUserLatLng;
+var followUser = true;
 
-// 🔹 Hentikan auto-follow bila pengguna interaksi dengan map
-map.on('mousedown touchstart', function() {
-  followUser = false;
-});
+map.on('mousedown touchstart', function() { followUser = false; });
 
-// Bila lokasi dijumpai
 function onLocationFound(e) {
   var latlng = e.latlng;
-  var radius = e.accuracy;
+  var radius = Math.min(e.accuracy, 100);
 
-  // Limit radius maksimum supaya bulatan tidak terlalu besar
-  if (radius > 100) radius = 100;
-
-  currentUserLatLng = latlng;
-
-  // Marker pengguna
-  if (!userMarker) {
-    userMarker = L.marker(latlng).addTo(map)
-      .bindPopup("📍 Lokasi Anda");
+  // Jika userMarker sudah ada, update lokasi sahaja
+  if(!userMarker) {
+    userMarker = L.marker(latlng).addTo(map).bindPopup("📍 Lokasi Anda");
   } else {
     userMarker.setLatLng(latlng);
   }
 
   // Bulatan ketepatan GPS
-  if (!accuracyCircle) {
+  if(!accuracyCircle) {
     accuracyCircle = L.circle(latlng, {
       radius: radius,
       color: '#136aec',
@@ -180,29 +209,14 @@ function onLocationFound(e) {
     accuracyCircle.setLatLng(latlng).setRadius(radius);
   }
 
-  // Auto-zoom hanya jika followUser = true
-  if (followUser) {
-    map.flyTo(latlng, 18, {
-      animate: true,
-      duration: 1.2
-    });
+  if(followUser) {
+    map.flyTo(latlng, 18, { animate:true, duration:1.2 });
   }
 }
 
-// Jika lokasi gagal
-function onLocationError(e) {
-  alert("Sila benarkan akses lokasi untuk menggunakan fungsi ini.");
-}
+function onLocationError(e) { alert("Sila benarkan akses lokasi untuk menggunakan fungsi ini."); }
 
-// Aktifkan LIVE tracking
-map.locate({
-  watch: true,                 // 🔥 real-time
-  setView: false,
-  maxZoom: 20,
-  minZoom: 5,
-  enableHighAccuracy: true
-});
-
+map.locate({ watch:true, setView:false, maxZoom:19, minZoom:5, enableHighAccuracy:true });
 map.on('locationfound', onLocationFound);
 map.on('locationerror', onLocationError);
 
